66723e867535cab5f30dfaf2af2c445ac635c500
erdeidominik99 <erdeidominik99@gmail.com>
2022-04-28 12:51:14 +0200

    binding

--- FriendshipExploder/GameOver/GameOverViewModel.cs
+++ FriendshipExploder/GameOver/GameOverViewModel.cs
@@ -1,22 +1,36 @@
 ﻿using FriendshipExploder.Logic;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace FriendshipExploder.GameOver
 {
-    public class GameOverViewModel
+    public class GameOverViewModel : ObservableRecipient
     {
         IGameModel logic;
 
         public Brush GameOverBackground { get; set; }
+        public ICommand BackCommand { get; set; }
+
+        public string FirstName { get; set; }
         public BitmapImage FirstImage { get; set; }
+        public string FirstKill { get; set; }
+
+        public string SecondName { get; set; }
         public BitmapImage SecondImage { get; set; }
+        public string SecondKill { get; set; }
+
+        public string ThirdName { get; set; }
         public BitmapImage ThirdImage { get; set; }
+        public string ThirdKill { get; set; }
+
 
         public GameOverViewModel()
         {
@@ -24,6 +38,13 @@ namespace FriendshipExploder.GameOver
             FirstImage = new BitmapImage(new Uri($"pack://application:,,,/Images/Players/0_player_down.png"));
             SecondImage = new BitmapImage(new Uri($"pack://application:,,,/Images/Players/2_player_down.png"));
             ThirdImage = new BitmapImage(new Uri($"pack://application:,,,/Images/Players/1_player_down.png"));
+
+            FirstKill = "10";
+
+            BackCommand = new RelayCommand(() =>
+            {
+                
+            });
         }
 
         public void SetupLogic(IGameModel model)
